9e8ad9aaffc0e91a288dc7ba5bfcd697106867bc
xogk1128 <xogk1128@naver.com>
2023-11-06 03:00:50 +0900

    회원가입에 keywords 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, UserProfile, Keyword
 # Register your models here.
-admin.site.register(User, UserAdmin)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [UserProfileInline]
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Keyword)  # Keyword 모델을 admin 페이지에 등록
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from .managers import UserManager
 
@@ -16,9 +18,22 @@ class User(AbstractUser):
     # updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
     def __str__(self):
         return self.email
+
+class Keyword(models.Model):
+    keyword = models.CharField(max_length=255, unique=True)
+    def __str__(self):
+        return self.keyword
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    keywords = models.ManyToManyField(Keyword)
+    def __str__(self):
+        return ', '.join([keyword.keyword for keyword in self.keywords.all()])
+
+
