27c2229d4fff0e3cd8088d534cb9ddedd0680af2
narae18 <starestarstar@naver.com>
2023-11-06 15:32:54 +0900

    feat: 내가쓴회고록 모델구성

--- myfarewell/models.py
+++ myfarewell/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Question(models.Model):
+    text = models.CharField(max_length=200, unique=True) 
+    available_after = models.DateField()  # 답변 가능한 날짜??....
+
+    def __str__(self):
+        return self.text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    answer_content = models.TextField(max_length=500,null=False,blank=False)  # 사용자의 답변
+    answered_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f"Answer by {self.user.username} to '{self.question.text}'"
