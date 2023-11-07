4cc8ffe36306870d225ef93289d4e1567a8b99cd
narae18 <starestarstar@naver.com>
2023-11-07 23:24:06 +0900

    feat:질문 태그로 테마 묶기 DB구성

--- publicfarewell/models.py
+++ publicfarewell/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
 # Create your models here.
 def image_upload_path(instance, filename):
     return f'{instance.pk}/[filename]'
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=15, null=True, blank=True)
 
 class PublicFarewell(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,3 +13,10 @@ class PublicFarewell(models.Model):
     content = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
+    questions = models.TextField(max_length=500, null=True, blank=True)
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    # admin = models.ForeignKey(User, on_delete=models.CASCADE)
+# class PublicFarewell_Q(models.Model):
+#     question = models.TextField(max_length=500)
+#     tag =
