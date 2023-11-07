37123a7fb23dd4cf235ed2a332dee8a753464f44
narae18 <starestarstar@naver.com>
2023-11-07 21:57:26 +0900

    feat:이미지필드수정

--- publicfarewell/models.py
+++ publicfarewell/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
+def image_upload_path(instance, filename):
+    return f'{instance.pk}/[filename]'
+
 class PublicFarewell(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=10)
     content = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
--- publicfarewell/serializers.py
+++ publicfarewell/serializers.py
@@ -6,3 +6,5 @@ class PublicFarewellSerializer(serializers.ModelSerializer):
     class Meta:
         model = PublicFarewell
         fields = '__all__'
+        
+    image = serializers.ImageField(use_url=True, required=False)
